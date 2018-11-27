348325930cce726772f2c98c2d8f2678f39e7d09
Yamid V.M <yavemu@gmail.com>
2018-11-27 18:44:18 -0500

    fix => optimizacion services del controlador UserList

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -2,12 +2,14 @@ angular
     .module('userList')
     .component('userList',{
         templateUrl: 'user-list/user-list.template.html',
-        controller: function UserListController($http) {
-            var self = this;
-            
-            $http.get('https://jsonplaceholder.typicode.com/users').then(function (response) {
-                self.users = response.data;
-            });
-            self.orderProp = 'name';
-        }
+        controller: ['$http',
+            function UserListController($http) {
+                var self = this;
+
+                $http.get('https://jsonplaceholder.typicode.com/users').then(function (response) {
+                    self.users = response.data;
+                });
+                self.orderProp = 'name';
+            }
+        ]
     })
